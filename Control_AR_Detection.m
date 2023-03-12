c2292963678978983d2e7e882c71986e61e843a8
Kaixin Liang <70785972+KC-leung233@users.noreply.github.com>
2023-03-12 15:12:18 +0800

    Update Control_AR_Detection.m

--- Control_AR_Detection.m
+++ Control_AR_Detection.m
@@ -1,29 +1,29 @@
 %% Step 1: Generate Potential AR Matrix
-% The original yearly vIVT matrix and the threshold (we use 98th percentile
-% of monthly climatology here) are needed before generating the monthly
-% potential vIVT-based ARs.The user can modify the detection scheme by
-% using different original data or threshold.
+% The original yearly IVT matrix and the threshold (we use 98th percentile
+% of monthly climatology and >50 kgm-1s-1 here) are needed before 
+% generating the monthly potential vIVT-based ARs.The user can modify 
+% the detection scheme by using different original data or threshold.
 clear; clc;
 % Select a period you want to generate the potential ARs
 Total_Detect_Series = ...
     datevec(datetime(1979, 1, 1, 0, 0, 0) : hours(6): datetime(2020, 12, 31, 23, 0, 0));
 Total_yr = unique(Total_Detect_Series(:, 1));
-load ERA5vIVT_98th_2020.mat % Load the threshold of ARs
+load ERA5IVT_98th_2020.mat % Load the threshold of ARs
 data_input_path = ''; % The path of your original data
 data_save_path = ''; % The path of your output data
 for yr = 1 : length(Total_yr)
-    vIVT_Total = load(strcat(data_input_path, string(Total_yr(yr)), '.mat'),'vIVT'); % Load the original yearly vIVT matrix
+    IVT_Total = load(strcat(data_input_path, string(Total_yr(yr)), '.mat'),'IVT'); % Load the original yearly IVT matrix
     Part_Detect_Series = Total_Detect_Series(Total_Detect_Series(:, 1) == Total_yr(yr), 2);
     Total_mo = unique(Part_Detect_Series);
     for mo  = 1 : length(Total_mo)
-        vIVT = vIVT_Total(:, :, Part_Detect_Series == Total_mo(mo)); % Archive the vIVT for a specific month
+        vIVT = vIVT_Total(:, :, Part_Detect_Series == Total_mo(mo)); % Archive the IVT for a specific month
         vIVT1 = vIVT;
         % Set the potential AR pixels into 1, and the non-AR pixels are 0
         vIVT1(vIVT < vIVT_98th(:, :, Total_mo(mo))) = 0;
         vIVT1(vIVT >= vIVT_98th(:, :, Total_mo(mo))) = 1;
-        % Save the monthly original vIVT and the potential ARs
-        save(strcat(data_save_path, 'vIVT_', ...
-            datestr(datetime(Total_yr(yr), Total_mo(mo), 1), 'yyyymm'), '.mat'), 'vIVT', 'vIVT1'); 
+        % Save the monthly original IVT and the potential ARs
+        save(strcat(data_save_path, 'IVT_', ...
+            datestr(datetime(Total_yr(yr), Total_mo(mo), 1), 'yyyymm'), '.mat'), 'IVT', 'IVT1'); 
     end
 end
 %% Step 2: Load basic dataset
@@ -62,7 +62,7 @@ for yr = 1 : length(Total_yr)
             datetime(Total_Detect_Series(Total_Detect_Series(:, 1) == Total_yr(yr) &...
             Total_Detect_Series(:, 2) == Total_mo(mo), :));
         % Load the potential AR matrix derived in Step 1
-        load(strcat(data_save_path, 'vIVT_', ...
+        load(strcat(data_save_path, 'IVT_', ...
             datestr(datetime(Total_yr(yr), Total_mo(mo), 1), 'yyyymm'), '.mat'), 'IVT', 'IVT1');
         if Rerun == 1
             [AR_Result, AR_IndResult, AR_Rerun_Index, AR_Rerun_Num, Max_Rerun_Num] = ...
